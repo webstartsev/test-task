d5f90219a0359f9baf0920b9b781daddd787dbde
Sergey Startsev <sergey@webstartsev.ru>
2020-04-21 14:54:38 +0300

    init

---
+++ src/components/abstract-component.js
@@ -0,0 +1,27 @@
+import {createElement} from '../utils/render';
+
+export default class AbstractComponent {
+  constructor() {
+    if (new.target === AbstractComponent) {
+      throw new Error(`Нельзя создавать экземляр AbstractComponent`);
+    }
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error(`Абстрактный метод не реализован: getTemplate`);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map`,
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    publicPath: `http://localhost:8080/`,
+    compress: true,
+    watchContentBase: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [`style-loader`, `css-loader`]
+      },
+      {
+        test: /.(jpg|jpeg|png|svg)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
